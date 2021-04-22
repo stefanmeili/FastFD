486a1435c677fd35fdfd30ee3d3f13f90480bb36
Stefan Meili <stefan.meili@gmail.com>
2021-04-22 14:43:43 -0700

    minor updates

--- setup.py
+++ setup.py
@@ -27,9 +27,12 @@ setuptools.setup(
     },
     classifiers = [
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     packages = setuptools.find_packages(),
+    install_requires=["numpy", "scipy"],
     python_requires = '>= 3.6',
+    include_package_data=True,
 )
